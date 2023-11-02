8907c4bb953c048f2f6a8bc4ec4d58706759f880
Sherub Thakur <sherub.thakur@gmail.com>
2023-11-02 14:53:42 +0530

    Setup rust project

---
+++ solutions/rust/01-init/code/src/main.rs
@@ -0,0 +1,23 @@
+use std::net::UdpSocket;
+
+fn main() {
+    let udp_socket = UdpSocket::bind("127.0.0.1:2053").expect("Failed to bind to address");
+    let mut buf = [0; 512];
+
+    loop {
+        match udp_socket.recv_from(&mut buf) {
+            Ok((size, source)) => {
+                let _received_data = String::from_utf8_lossy(&buf[0..size]);
+                println!("Received {} bytes from {}", size, source);
+                let response = [];
+                udp_socket
+                    .send_to(&response, source)
+                    .expect("Failed to send response");
+            }
+            Err(e) => {
+                eprintln!("Error receiving data: {}", e);
+                break;
+            }
+        }
+    }
+}
