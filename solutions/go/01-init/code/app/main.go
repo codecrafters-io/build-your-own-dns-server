7ee22da548ad7278bd9dd7f0f155f4f284e7c4e9
Sherub Thakur <sherub.thakur@gmail.com>
2023-11-03 16:18:26 +0530

    Setup go project

---
+++ solutions/go/01-init/code/app/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	udpAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:2053")
+	if err != nil {
+		fmt.Println("Failed to resolve UDP address:", err)
+		return
+	}
+
+	udpConn, err := net.ListenUDP("udp", udpAddr)
+	if err != nil {
+		fmt.Println("Failed to bind to address:", err)
+		return
+	}
+	defer udpConn.Close()
+
+	buf := make([]byte, 512)
+
+	for {
+		size, source, err := udpConn.ReadFromUDP(buf)
+		if err != nil {
+			fmt.Println("Error receiving data:", err)
+			break
+		}
+
+		receivedData := string(buf[:size])
+		fmt.Printf("Received %d bytes from %s: %s\n", size, source, receivedData)
+
+		// Create an empty response
+		response := []byte{}
+
+		_, err = udpConn.WriteToUDP(response, source)
+		if err != nil {
+			fmt.Println("Failed to send response:", err)
+		}
+	}
+}
