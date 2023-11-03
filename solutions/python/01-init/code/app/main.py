7d2ac25f9d9d781b3b628d3bd58a2799ab8167de
Sherub Thakur <sherub.thakur@gmail.com>
2023-11-03 15:51:11 +0530

    Setup python project

---
+++ solutions/python/01-init/code/app/main.py
@@ -0,0 +1,21 @@
+import socket
+
+
+def main():
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_socket.bind(("127.0.0.1", 2053))
+
+    while True:
+        try:
+            buf, source = udp_socket.recvfrom(512)
+
+            response = b""
+
+            udp_socket.sendto(response, source)
+        except Exception as e:
+            print(f"Error receiving data: {e}")
+            break
+
+
+if __name__ == "__main__":
+    main()
