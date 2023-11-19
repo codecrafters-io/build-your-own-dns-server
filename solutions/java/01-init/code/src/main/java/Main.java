0c9439acec5a0bfad1f8164c4f507b2c4698ea6f
Uriel Salischiker <uriels@spotify.com>
2023-11-19 02:37:44 +0100

    Add java support

---
+++ solutions/java/01-init/code/src/main/java/Main.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class Main {
+  public static void main(String[] args){
+    int port = 2053;
+    while(true) {
+      try(DatagramSocket serverSocket = new DatagramSocket(port)) {
+        final byte[] buf = new byte[512];
+        final DatagramPacket packet = new DatagramPacket(buf, buf.length);
+        serverSocket.receive(packet);
+        System.out.println("Received " + new String(buf) + " from " + serverSocket.getInetAddress());
+
+        final byte[] bufResponse = new byte[512];
+        final DatagramPacket packetResponse = new DatagramPacket(bufResponse, bufResponse.length);
+        serverSocket.send(packetResponse);
+      } catch (IOException e) {
+        System.out.println("IOException: " + e.getMessage());
+      }
+    }
+  }
+}
