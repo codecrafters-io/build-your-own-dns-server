944e50dc583394af996a62ed8b588c0c87d9a62e
Sherub Thakur <sherub.thakur@gmail.com>
2023-12-18 14:35:11 +0530

    JS support

---
+++ solutions/javascript/01-init/code/app/main.js
@@ -0,0 +1,22 @@
+const dgram = require("dgram");
+
+const udpSocket = dgram.createSocket("udp4");
+udpSocket.bind(2053, "127.0.0.1");
+
+udpSocket.on("message", (buf, rinfo) => {
+  try {
+    const response = Buffer.from("");
+    udpSocket.send(response, rinfo.port, rinfo.address);
+  } catch (e) {
+    console.log(`Error receiving data: ${e}`);
+  }
+});
+
+udpSocket.on("error", (err) => {
+  console.log(`Error: ${err}`);
+});
+
+udpSocket.on("listening", () => {
+  const address = udpSocket.address();
+  console.log(`Server listening ${address.address}:${address.port}`);
+});
