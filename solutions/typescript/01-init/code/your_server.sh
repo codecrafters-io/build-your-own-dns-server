5833e73a000c074ed3953755da65c2eee2ee3628
Ryan Gang <ryan-gg@outlook.com>
2024-04-19 13:58:57 +0530

    feat: remove deno cache from spawn script

--- solutions/typescript/01-init/code/your_server.sh
+++ solutions/typescript/01-init/code/your_server.sh
@@ -5,7 +5,6 @@
 # CodeCrafters uses this file to test your code. Don't make any changes here!
 #
 # DON'T EDIT THIS!
-deno cache app/main.ts
 # Deno.listenDatagram is still unstable as of Deno 1.42.4
 # https://docs.deno.com/runtime/manual/tools/unstable_flags#--unstable-net
 exec deno run --allow-net --allow-read --allow-write --unstable-net app/main.ts "$@"
