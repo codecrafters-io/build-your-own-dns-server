4ca34b6146001897c22fcc3aa9b8dc9c7356d821
Andy Li <1450947+andy1li@users.noreply.github.com>
2025-10-07 15:53:57 +0900

    Update Kotlin project structure and dependencies
    - Refactored project to use Gradle for building and running the application.
    - Changed entry point from `src/main/kotlin/Main.kt` to `app/src/main/kotlin/App.kt`.
    - Updated `.gitignore` files to include Gradle-specific entries.
    - Modified `codecrafters.yml` to use Kotlin version 2.2.
    - Added necessary Gradle configuration files and scripts for building and running the application.
    - Updated README and other documentation to reflect the new structure and requirements.

--- compiled_starters/kotlin/your_program.sh
+++ compiled_starters/kotlin/your_program.sh
@@ -14,11 +14,14 @@ set -e # Exit early if any commands fail
 # - Edit .codecrafters/compile.sh to change how your program compiles remotely
 (
   cd "$(dirname "$0")" # Ensure compile steps are run within the repository directory
-  mvn -B package -Ddir=/tmp/codecrafters-build-dns-server-kotlin
+  gradle distTar
+  cd /tmp/codecrafters-build-dns-server-kotlin/distributions
+  rm -rf app
+  tar -xvf app.tar
 )
 
 # Copied from .codecrafters/run.sh
 #
 # - Edit this to change how your program runs locally
 # - Edit .codecrafters/run.sh to change how your program runs remotely
-exec java -jar /tmp/codecrafters-build-dns-server-kotlin/build-your-own-dns-server.jar "$@"
+exec /tmp/codecrafters-build-dns-server-kotlin/distributions/app/bin/app "$@"
--- starter_templates/kotlin/code/.codecrafters/compile.sh
+++ starter_templates/kotlin/code/.codecrafters/compile.sh
@@ -8,4 +8,7 @@
 
 set -e # Exit on failure
 
-mvn -B package -Ddir=/tmp/codecrafters-build-dns-server-kotlin
+gradle distTar
+cd /tmp/codecrafters-build-dns-server-kotlin/distributions
+rm -rf app
+tar -xvf app.tar
