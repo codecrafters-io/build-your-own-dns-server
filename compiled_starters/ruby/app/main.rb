3af9f43d51a5464fbf286eef5fe241c46efc0cf3
Andy Li <1450947+andy1li@users.noreply.github.com>
2025-12-10 01:10:15 +0900

    Refactor Ruby UDP server implementation: move server logic from server.rb to main.rb, update logging method, and add error handling for data reception.

--- compiled_starters/ruby/app/main.rb
+++ compiled_starters/ruby/app/main.rb
@@ -1,5 +1,18 @@
-# You can use print statements as follows for debugging, they'll be visible when running tests.
-$stderr.puts "Logs from your program will appear here!"
+require "socket"
 
-# Uncomment this block to pass the first stage
-# puts "Implement code here!"
+udp_socket = UDPSocket.new
+udp_socket.bind("127.0.0.1", 2053)
+
+# You can use print statements as follows for debugging, they'll be visible when running tests.
+print("Logs from your program will appear here!")
+
+# TODO: Uncomment the code below to pass the first stage
+#
+# loop do
+#   data, (_, sender_port, _, sender_ip) = udp_socket.recvfrom(512)
+#   response = ""
+#   udp_socket.send(response, 0, sender_ip, sender_port)
+# rescue => e
+#   puts "Error receiving data: #{e}"
+#   break
+# end
