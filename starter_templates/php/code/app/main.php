2626a80f45287d79f7143be3c8245c16c31e6d17
Andy Li <1450947+andy1li@users.noreply.github.com>
2026-04-28 07:47:54 +0900

    Add PHP starter template and solution for DNS server challenge
    - Created starter template files including README, configuration, and scripts for local execution.
    - Added initial PHP application structure with socket setup for DNS server functionality.
    - Included Codecrafters configuration files for build and run processes.
    - Established a sample solution for the "Build Your Own DNS server" challenge with necessary files and instructions.

---
+++ starter_templates/php/code/app/main.php
@@ -0,0 +1,25 @@
+<?php
+error_reporting(E_ALL);
+
+$udp_socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+
+// Since the tester restarts your program quite often, setting SO_REUSEADDR
+// ensures that we don't run into 'Address already in use' errors
+socket_set_option($udp_socket, SOL_SOCKET, SO_REUSEADDR, 1);
+
+socket_bind($udp_socket, '127.0.0.1', 2053);
+
+// You can use print statements as follows for debugging, they'll be visible when running tests.
+fwrite(STDERR, "Logs from your program will appear here!\n");
+
+// TODO: Uncomment the code below to pass the first stage
+// 
+// while (true) {
+//     $buf = $from = '';
+//     $port = 0;
+//     if (@socket_recvfrom($udp_socket, $buf, 512, 0, $from, $port) === false) {
+//         fwrite(STDERR, 'Error receiving data: ' . socket_strerror(socket_last_error($udp_socket)) . "\n");
+//         break;
+//     }
+//     socket_sendto($udp_socket, '', 0, 0, $from, $port);
+// }
